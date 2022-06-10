59508deabdb29ce8cb40ec7a2b545f67201564eb
Jontaybr <Jontaybr@gmail.com>
2022-06-10 11:13:03 -0600

    added a script to the square

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private float movespeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(movespeed * Time.deltaTime, 0, 0);
+    }
+}
